379d8113e71a1ea567b1cac8e4609d479eca849e
Jason Henline <jason.henline@gmail.com>
2014-05-15 12:10:50 -0700

    Display the role assignment
    When 2 unique players have entered a room, perform the role assignment and
    display to each player his role.

--- avalon/model.py
+++ avalon/model.py
@@ -1,3 +1,5 @@
+import random
+import uuid
 from google.appengine.ext import ndb
 
 ROLES = ['Minion', 'Merlin', 'Loyal', 'Mordred', 'Morgana', 'Oberon', 'Percival']
@@ -7,36 +9,52 @@ class RoleAssignment(ndb.Model):
     user = ndb.UserProperty(required=True)
     role = ndb.StringProperty(choices=ROLES, required=True)
 
-    @classmethod
-    def GetKey(cls, assignment_id):
-        return ndb.Key('role_assignment', assignment_id)
-
 
 class GameState(ndb.Model):
+    game_id = ndb.StringProperty(required=True)
     assignments = ndb.StructuredProperty(RoleAssignment, repeated=True)
-
-    @classmethod
-    def Query(cls, game_id):
-        return _GetSingleResult(cls.query(ancestor=cls.GetKey(game_id)).fetch(1))
-
-    @classmethod
-    def GetKey(cls, game_id):
-        return ndb.Key('game_state', game_id)
 
 
 class Room(ndb.Model):
-    room_name = ndb.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
     users = ndb.UserProperty(repeated=True)
     game_state = ndb.StructuredProperty(GameState)
 
-    @classmethod
-    def Query(cls, room_name):
-        return _GetSingleResult(cls.query(ancestor=cls.GetKey(room_name)).fetch(1))
 
-    @classmethod
-    def GetKey(cls, room_name):
-        return ndb.Key('room', room_name)
-    
+def getRoom(room_name):
+    return ndb.Key(Room, room_name).get()
 
-def _GetSingleResult(l):
-    return l[0] if l else None
+@ndb.transactional
+def addUserToRoom(room_name, user):
+    added = False
+    room = getRoom(room_name)
+    if not room:
+        room = Room(name=room_name, id=room_name)
+    if user not in room.users:
+        room.users.append(user)
+        added = True
+    room.put()
+    return added
+
+@ndb.transactional
+def getGameInfo(room_name):
+    retval = {'ready': 'False'}
+    room = getRoom(room_name)
+    if room and len(room.users) >= 2:
+        retval['ready'] = True
+        if not room.game_state:
+            game_state = GameState(game_id=uuid.uuid4().hex)
+            available_roles = ROLES[:]
+            for user in room.users:
+                role = random.choice(available_roles)
+                available_roles.remove(role)
+                assignment = RoleAssignment()
+                assignment.user = user
+                assignment.role = role
+                game_state.assignments.append(assignment)
+            room.game_state = game_state
+            room.put()
+        retval['assignments'] = room.game_state.assignments
+    if room:
+        retval['number_of_users'] = len(room.users)
+    return retval
--- avalon/room.py
+++ avalon/room.py
@@ -2,9 +2,6 @@ import jinja2
 import json
 import os
 import model
-from model import GameState, RoleAssignment, Room
-import random
-import uuid
 import webapp2
 
 from google.appengine.api import users
@@ -21,46 +18,30 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class RoomPage(webapp2.RequestHandler):
     
     def get(self, room_name):
-        user = users.get_current_user();
+        user = users.get_current_user()
         if not user:
-            self.redirect(users.create_login_url(self.request.uri))
-            return
-        
+            return self.redirect(users.create_login_url(self.request.uri))
+
+        model.addUserToRoom(room_name, user)
+
         template = JINJA_ENVIRONMENT.get_template('room.html')
         self.response.write(template.render({'room_name': room_name}))
 
-        room = Room.Query(room_name)
-        if not room:
-            room = Room(parent=Room.GetKey(room_name))
-            room.room_name = room_name
-        room.users.append(user)
-        room.put()
-        
 
 class RoomStatusPage(webapp2.RequestHandler):
     
     def get(self, room_name):
-        room = Room.Query(room_name)
-        json_data = {'ready': False}
-        if self._IsReady(room):
-            if room.game_state is None:
-                room.game_state = self._CreateNewGameState(room)
-                room.put()
-            json_data['ready'] = True
-        self.response.write(json.dumps(json_data))
-
-    def _IsReady(self, room):
-        return room and len(room.users) >= 2
-
-    def _CreateNewGameState(self, room):
-        game_state = GameState(parent=GameState.GetKey(uuid.uuid4().hex))
-        for user in room.users:
-            assignment = RoleAssignment(parent=RoleAssignment.GetKey(uuid.uuid4().hex))
-            assignment.user = user
-            assignment.role = random.choice(model.ROLES)
-            game_state.assignments.append(assignment)
-        return game_state
-
+        user = users.get_current_user()
+        game_info = model.getGameInfo(room_name)
+        if 'assignments' in game_info:
+            for assignment in game_info['assignments']:
+                if assignment.user == user:
+                    role = assignment.role
+                    break
+            del game_info['assignments']
+            game_info['role'] = role
+        self.response.write(json.dumps(game_info))
+    
 
 application = webapp2.WSGIApplication([
     (r'/(\w+)', RoomPage),
