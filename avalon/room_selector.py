a3b60af62dd0571ee54ff0f2b8aecdb73c5c29a0
Jason Henline <jason.henline@gmail.com>
2014-05-15 09:37:46 -0700

    Simplify room redirection
    Instead of using JavaScript to set the action of the html form dynamically,
    just use a hard-coded action URI and set up a handler on that static URI to
    redirect to the room-specific URI based on the values in the form.

--- avalon/room_selector.py
+++ avalon/room_selector.py
@@ -1,3 +1,4 @@
+import uuid
 import webapp2
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -5,27 +6,28 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 HTML = """
 <!doctype html>
 <html>
+  <head>
+    <title>Avalon</title>
+  </head>
   <body>
-    <form method="get" id="room_selector" >
-      Room: <input type="text" id="room_id">
-      <input type="submit" id="room_submit" value="Join">
+    <form method="post" action="/" >
+      Enter Room Name: <input type="text" name="room_name">
+      <input type="submit" value="Join">
     </form>
-
-    <script>
-      document.getElementById('room_submit').onclick = function() {
-        document.forms[0].action = document.getElementById('room_id').value;
-      }
-    </script>
   </body>
 </html>
 """
 
 class MainPage(webapp2.RequestHandler):
     
-        
     def get(self):
-#         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(HTML)
+
+    def post(self):
+        room_name = self.request.get('room_name')
+        if not room_name:
+            room_name = uuid.uuid4().hex
+        self.redirect('/' + room_name)
 
 
 application = webapp2.WSGIApplication([('/', MainPage)], debug=True)
