a8bd099f9e9729c847457973a86e385c7860da3f
Jason Henline <jason.henline@gmail.com>
2014-05-18 22:14:40 -0700

    Refactoring through game creation
    Using channels instead of polling. Also refactoring the code to make it easier
    to read and use. One big feature change is that the game creator now chooses
    who gets to play. Things seem to be working for game creation. The next thing
    to do is to make sure the game itself is working.

--- avalon/model.py
+++ avalon/model.py
@@ -1,10 +1,13 @@
+import json
 import random
+
+from google.appengine.api import channel
 from google.appengine.ext import ndb
 
-GOOD_SPECIAL_ROLES = ['Merlin', 'Percival']
-EVIL_SPECIAL_ROLES = ['Mordred', 'Morgana', 'Oberon']
+GOOD_SPECIAL_ROLES = ['merlin', 'percival']
+EVIL_SPECIAL_ROLES = ['mordred', 'morgana', 'oberon']
 SPECIAL_ROLES = [role for group in (GOOD_SPECIAL_ROLES, EVIL_SPECIAL_ROLES) for role in group]
-ROLES = [role for group in (['Minion', 'Loyal'], SPECIAL_ROLES) for role in group]
+ROLES = [role for group in (['minion', 'loyal'], SPECIAL_ROLES) for role in group]
 
 ROOM_STATES = ['NO_GAME', 'GAME_BEING_CREATED', 'GAME_IN_PROGRESS']
 
@@ -22,50 +25,95 @@ MISSION_PARAMETERS = {5: [[2, 1], [3, 1], [2, 1], [3, 1], [3, 1]],
               10: [[3, 1], [4, 1], [4, 1], [5, 2], [5, 1]]}
 
 
-class RoomState(object):
-    pass
-    # TODO encapsulate one of ROOM_STATES plus a list of users waiting in the room.
-    # Send this as the responsge to GetRoomState
-
-
 class RoleAssignment(ndb.Model):
     user = ndb.UserProperty(required=True)
     role = ndb.StringProperty(choices=ROLES, required=True)
 
 
-class Vote(ndb.Model):
+class BooleanVote(ndb.Model):
     user = ndb.UserProperty(required=True)
     vote = ndb.BooleanProperty(required=True)
 
 
 class Game(ndb.Model):
-    player_count = ndb.IntegerProperty(required=True)
-    available_roles = ndb.StringProperty(choices=ROLES, repeated=True)
-    all_roles = ndb.StringProperty(choices=ROLES, repeated=True)
+    players = ndb.UserProperty(repeated=True)
+    roles = ndb.StringProperty(choices=ROLES, repeated=True)
     assignments = ndb.StructuredProperty(RoleAssignment, repeated=True)
-    owner = ndb.UserProperty(required=True)
+    
     leader_index = ndb.IntegerProperty()
     round_number = ndb.IntegerProperty()
     round_state = ndb.StringProperty(choices=ROUND_STATES)
     round_failed_leader_count = ndb.IntegerProperty()
     team_proposal = ndb.StringProperty(repeated=True)
-    team_proposal_votes = ndb.StructuredProperty(Vote, repeated=True)
+    team_proposal_votes = ndb.StructuredProperty(BooleanVote, repeated=True)
     team = ndb.StringProperty(repeated=True)
-    mission_votes = ndb.StructuredProperty(Vote, repeated=True)
+    mission_votes = ndb.StructuredProperty(BooleanVote, repeated=True)
     number_of_missions_failed = ndb.IntegerProperty()
+    
+    def includes_user(self, user):
+        return user in self.players
 
 
 class Room(ndb.Model):
     state = ndb.StringProperty(choices=ROOM_STATES, required=True)
+    users = ndb.UserProperty(repeated=True)
+    owner = ndb.UserProperty()
     game = ndb.StructuredProperty(Game)
-
-
-@ndb.transactional
-def getRoom(room_name):
-    room = ndb.Key(Room, room_name).get()
-    if not room:
-        room = Room(id=room_name, state='NO_GAME')
-    return room
+    
+    @staticmethod
+    def get(room_name):
+        room = ndb.Key(Room, room_name).get()
+        if not room:
+            room = Room(id=room_name, state='NO_GAME')
+        return room
+    
+    @staticmethod
+    def notify_all(room_name):
+        room = Room.get(room_name)
+        owner_nickname = None
+        if room.owner:
+            owner_nickname = room.owner.nickname()
+        message = {'room_state': room.state,
+                   'nicknames_present': [u.nickname() for u in room.users],
+                   'owner_nickname': owner_nickname}
+        for user in room.users:
+            if room.state == 'GAME_IN_PROGRESS' and room.game and user in room.game.players:
+                message['in_game'] = True
+            else:
+                message['in_game'] = False
+            channel.send_message(user.user_id() + room_name, json.dumps(message))
+    
+    @staticmethod
+    @ndb.transactional
+    def take_ownership(room_name, user):
+        room = Room.get(room_name)
+        if room.state == 'NO_GAME':
+            room.state = 'GAME_BEING_CREATED'
+            room.owner = user
+            room.put()
+            return True
+        else:
+            return room.owner == user
+    
+    @staticmethod
+    @ndb.transactional
+    def relinquish_ownership(room_name, user):
+        room = Room.get(room_name)
+        if room.owner == user:
+            room.owner = None
+            if room.state == 'GAME_BEING_CREATED':
+                room.state = 'NO_GAME'
+            room.put()
+    
+    def add_user(self, user):
+        if not self.users:
+            self.users = []
+        if not user in self.users:
+            self.users.append(user)
+    
+    def remove_user(self, user):
+        if user in self.users:
+            self.users.remove(user)
 
 
 @ndb.transactional
@@ -73,7 +121,7 @@ def addPlayerToGame(room_name, user):
     room = ndb.Key(Room, room_name).get()
     if not room or not room.game:
         return False
-    game = getRoom(room_name).game
+    game = Room.get(room_name).game
     already_present = False
     for assignment in game.assignments:
         if assignment.user == user:
@@ -97,22 +145,6 @@ def addPlayerToGame(room_name, user):
         game.number_of_missions_failed = 0
     room.put()
     return True
-
-
-@ndb.transactional
-def beginGameCreation(room_name, user):
-    room = ndb.Key(Room, room_name).get()
-    if not room:
-        room = Room(id=room_name, state='NO_GAME')
-    if room.state == 'NO_GAME':
-        room.state = 'GAME_BEING_CREATED'
-        room.game = Game(owner=user, player_count=DEFAULT_PLAYER_COUNT)
-        room.put()
-        return True
-    elif room.state == 'GAME_BEING_CREATED' and room.game.owner == user:
-        return True
-    else:
-        return False
 
 
 @ndb.transactional
--- avalon/room.py
+++ avalon/room.py
@@ -1,13 +1,15 @@
 import jinja2
 import json
+import logging
 import os
 import model
+import random
 import webapp2
 
 from google.appengine.api import channel
+from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
-from model import DEFAULT_PLAYER_COUNT
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -19,82 +21,133 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class RoomPage(webapp2.RequestHandler):
     
-    def get(self, room_name):
-        user = users.get_current_user()
-        if not user:
-            return self.redirect(users.create_login_url(self.request.uri))
-        
-        token = channel.create_channel(user.user_id() + room_name)
-        template_values = {'token': token,
-                           'room_name': room_name}
-
-#         room = model.getRoom(room_name)
-#         if room.game and room.game.assignments:
-#             for assignment in room.game.assignments:
-#                 if assignment.user == user:
-#                     return self.redirect('/' + room_name + '/game')
-
-        template = JINJA_ENVIRONMENT.get_template('room.html')
-        self.response.write(template.render(template_values))
-
-
-class RoomStatusPage(webapp2.RequestHandler):
+    @staticmethod
+    @ndb.transactional
+    def add_user(room_name, user):
+        room = model.Room.get(room_name)
+        room.add_user(user)
+        room.put()
     
     def get(self, room_name):
         user = users.get_current_user()
         if not user:
             return self.redirect(users.create_login_url(self.request.uri))
+        
+        room = model.Room.get(room_name)
+        if room.state == 'GAME_BEING_CREATED' and room.owner == user:
+            return self.redirect('/' + room_name + '/create_game');
+        elif room.game and room.game.includes_user(user):
+            return self.redirect('/' + room_name + '/game')
+        
+        RoomPage.add_user(room_name, user)
+        
+        token = channel.create_channel(user.user_id() + room_name)
+        template_values = {'token': token,
+                           'room_name': room_name}
 
-        state = model.getRoom(room_name).state
-        #self.response.write(state)
-        channel.send_message(user.user_id() + room_name, state)
+        template = JINJA_ENVIRONMENT.get_template('room.html')
+        self.response.write(template.render(template_values))
+    
+    def post(self, room_name):
+        model.Room.notify_all(room_name)
 
 
 class GameCreatePage(webapp2.RequestHandler):
     
     def get(self, room_name):
         user = users.get_current_user()
-        begun = model.beginGameCreation(room_name, user)
-        if not begun:
+        if not user:
+            return self.redirect(users.create_login_url(self.request.uri))
+        
+        if not model.Room.take_ownership(room_name=room_name, user=user):
             return self.redirect('/' + room_name)
+        
+        model.Room.notify_all(room_name)
+        
+        room = model.Room.get(room_name)
+        template_values = {'room_name': room_name,
+                           'nicknames': [u.nickname() for u in room.users]}
+        
         template = JINJA_ENVIRONMENT.get_template('create_game.html')
-        self.response.write(template.render({'room_name': room_name}))
+        self.response.write(template.render(template_values))
+
+    @staticmethod
+    def validate_form(special_role_count, evil_special_role_count, player_count, evil_role_count):
+        if special_role_count > player_count:
+            return False
+        if evil_special_role_count > evil_role_count:
+            return False
+        return True
+        
+
+    def post(self, room_name):
+        user = users.get_current_user()
+        if not user:
+            return self.redirect(users.create_login_url(self.request.uri))
+        
+        room = model.Room.get(room_name)
+        if room.owner != user:
+            return self.redirect('/' + room_name)
+        
+        roles = []
+        special_roles = []
+        nicknames = []
+        evil_role_count = 0
+        for arg in self.request.arguments():
+            if arg.endswith('_role') and self.request.get(arg) == 'on':
+                special_roles.append(arg[:-len('_role')])
+            elif arg.endswith('_name') and self.request.get(arg) == 'on':
+                nicknames.append(arg[:-len('_name')])
+            elif arg == 'evil_role_count':
+                evil_role_count = int(self.request.get(arg))
+        
+        evil_special_role_count = sum(1 for role in special_roles if role in model.EVIL_SPECIAL_ROLES)
+        if not GameCreatePage.validate_form(len(special_roles), evil_special_role_count, len(nicknames), evil_role_count):
+            return self.redirect('/' + room_name)
+        
+        minion_count = evil_role_count - evil_special_role_count
+        roles.extend(minion_count * ['minion'])
+        loyal_count = len(nicknames) - len(special_roles) - minion_count
+        roles.extend(loyal_count * ['loyal'])
+        roles.extend(special_roles)
+        
+        logging.critical(repr(nicknames))
+        
+        players = []
+        for nickname in nicknames:
+            for user in room.users:
+                if nickname == user.nickname():
+                    players.append(user)
+                    break
+        
+        random.shuffle(players)
+        
+        assignments = []
+        available_roles = roles[:]
+        for user in players:
+            role = random.choice(available_roles)
+            available_roles.remove(role)
+            assignments.append(model.RoleAssignment(user=user, role=role))
+        
+        logging.critical(repr(players))
+        
+        room.game = model.Game(players=players, roles=roles, assignments=assignments)
+        room.state = 'GAME_IN_PROGRESS'
+        room.put()
+        return self.redirect('/' + room_name)
+    
+
+class CancelGameCreatePage(webapp2.RequestHandler):
     
     def post(self, room_name):
         user = users.get_current_user()
-        room = model.getRoom(room_name)
-        if not room.game or room.game.owner != user:
-            return self.redirect('/' + room_name)
-        player_count = self.request.get('player_count')
-        if player_count:
-            player_count = int(player_count)
-        else:
-            player_count = DEFAULT_PLAYER_COUNT
-        room.game.player_count = player_count
-        good_player_count = 0
-        evil_player_count = 0
-        available_roles = []
-        for role in model.SPECIAL_ROLES:
-            if self.request.get(role.lower()):
-                available_roles.append(role)
-                if role in model.GOOD_SPECIAL_ROLES:
-                    good_player_count += 1
-                else:
-                    evil_player_count += 1
-        desired_evil_player_count = (player_count + 2) / 3
-        while evil_player_count < desired_evil_player_count and evil_player_count + good_player_count < player_count:
-            available_roles.append('Minion')
-            evil_player_count += 1
-        while evil_player_count + good_player_count < player_count:
-            available_roles.append('Loyal')
-            good_player_count += 1
-        room.game.available_roles = available_roles
-        room.game.all_roles = available_roles[:]
-        room.state = 'WAITING_FOR_PLAYERS'
-        room.put()
+        if not user:
+            return self.redirect(users.create_login_url(self.request.uri))
+        model.Room.relinquish_ownership(room_name, user)
         return self.redirect('/' + room_name)
 
 
+#TODO From here onwards.
 class GamePage(webapp2.RequestHandler):
     
     def get(self, room_name):
@@ -102,7 +155,7 @@ class GamePage(webapp2.RequestHandler):
         if not user:
             return self.redirect('/' + room_name)
         added = model.addPlayerToGame(room_name, user)
-        room = model.getRoom(room_name)
+        room = model.Room.get(room_name)
         if not room.game:
             return self.redirect('/' + room_name)
         assignments = room.game.assignments
@@ -121,7 +174,7 @@ class GameStatusPage(webapp2.RequestHandler):
     
     def get(self, room_name):
         user = users.get_current_user()
-        room = model.getRoom(room_name)
+        room = model.Room.get(room_name)
         info = {'identities': []}
         if room.game and room.game.assignments:
             if room.game.all_roles:
@@ -210,7 +263,7 @@ class SubmitTeamProposalPage(webapp2.RequestHandler):
     
     def post(self, room_name):
         user = users.get_current_user()
-        room = model.getRoom(room_name)
+        room = model.Room.get(room_name)
         if not user or not room or not room.game or room.game.assignments[room.game.leader_index].user != user:
             return self.redirect('/' + room_name)
         proposal = []
@@ -228,7 +281,7 @@ class VoteOnTeamProposal(webapp2.RequestHandler):
 
     def post(self, room_name):
         user = users.get_current_user()
-        room = model.getRoom(room_name)
+        room = model.Room.get(room_name)
         if not user or not room or not room.game or room.game.round_state != 'VOTING_ON_TEAM' or user in [v.user for v in room.game.team_proposal_votes]:
             return self.redirect('/' + room_name)
         if not room.game.team_proposal_votes:
@@ -236,7 +289,7 @@ class VoteOnTeamProposal(webapp2.RequestHandler):
         vote = False
         if self.request.get('team_proposal_vote') == 'yes':
             vote = True
-        room.game.team_proposal_votes.append(model.Vote(user=user, vote=vote))
+        room.game.team_proposal_votes.append(model.BooleanVote(user=user, vote=vote))
         if len(room.game.team_proposal_votes) == room.game.player_count:
             ayes = 0
             nays = 0
@@ -280,8 +333,8 @@ class GameDestroyPage(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
     (r'/(\w+)', RoomPage),
-    (r'/state/(\w+)', RoomStatusPage),
     (r'/(\w+)/create_game', GameCreatePage),
+    (r'/(\w+)/cancel_create_game', CancelGameCreatePage),
     (r'/(\w+)/destroy_game', GameDestroyPage),
     (r'/(\w+)/game', GamePage),
     (r'/(\w+)/game_state', GameStatusPage),
